5bc0b83437fed4702a3599d43747ef67de622b7d
MadnessPray <ilyashem94@gmail.com>
2013-12-03 23:09:06 +0400

    test

---
+++ src/com/example/helloAndroid/MyActivity.java
@@ -0,0 +1,28 @@
+package com.example.helloAndroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void onClick(View v)
+    {
+        EditText edit = (EditText) findViewById(R.id.editText);
+
+        Intent intent = new Intent(MyActivity.this, AboutActivity.class);
+        intent.putExtra("key", edit.getText().toString());
+
+        startActivity(intent);
+    }
+}
